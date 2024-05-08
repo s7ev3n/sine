0bb4577d79b25ece62abd2c2d42ce3d1075a72e4
s7ev3n <s.eleven.73@gmail.com>
2024-05-08 08:45:50 +0800

    add deepseek model

--- src/sine/models/api_model.py
+++ src/sine/models/api_model.py
@@ -7,19 +7,25 @@ AVAILABLE_API_MODELS = {
         "mixtral-8x7b-32768", "llama2-70b-4096", "llama3-70b-8192", "llama3-8b-8192"
     ],
     'zhipuai' : ["glm-4"],
-    'moonshot' : ["moonshot-v1-32k", "moonshot-v1-8k"]
+    'moonshot' : ["moonshot-v1-32k", "moonshot-v1-8k"],
+    'deepseek' : ["deepseek-chat", "deepseek-coder"]
 }
 
 def get_api_model(model_name):
     if 'glm' in model_name:
         from zhipuai import ZhipuAI
-        client = ZhipuAI(api_key=os.getenv("ZHIPU_API_KEY"))
         assert model_name in AVAILABLE_API_MODELS['zhipu']
+        client = ZhipuAI(api_key=os.getenv("ZHIPU_API_KEY"))       
     elif 'moonshot' in model_name:
         from openai import OpenAI
+        assert model_name in AVAILABLE_API_MODELS['moonshot']
         client = OpenAI(api_key=os.getenv("MOONSHOT_API_KEY"),
                         base_url="https://api.moonshot.cn/v1")
-        assert model_name in AVAILABLE_API_MODELS['moonshot']
+    elif 'deepseek' in model_name:
+        from openai import OpenAI
+        assert model_name in AVAILABLE_API_MODELS['deepseek']
+        client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"),
+                        base_url="https://api.deepseek.com")     
     elif model_name in AVAILABLE_API_MODELS['groq']:
         from groq import Groq
         client = Groq(api_key=os.getenv("GROQ_API_KEY"))
