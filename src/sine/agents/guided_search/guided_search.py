7a946f5838f780d8795d7ecb47a526f044a1979d
s7ev3n <s.eleven.73@gmail.com>
2024-07-24 21:12:52 +0800

    update langgraph to achieve human-in-the-loop for user profile

--- src/sine/agents/guided_search/guided_search.py
+++ src/sine/agents/guided_search/guided_search.py
@@ -1,6 +1,7 @@
 import os
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.pydantic_v1 import BaseModel
 from langchain_groq import ChatGroq
 from sine.agents.guided_search.prompts import GET_USER_PROFILE
 from typing import Annotated, List
@@ -14,7 +15,7 @@ from langgraph.checkpoint.sqlite import SqliteSaver
 
 def init_new_llm():
     return ChatGroq(api_key=os.getenv("GROQ_API_KEY"), 
-                    model="llama3-groq-70b-8192-tool-use-preview")
+                    model="llama3-70b-8192")
 
 llm = init_new_llm()
 class State(TypedDict):
@@ -30,51 +31,48 @@ gen_user_profile = ChatPromptTemplate.from_messages([
     MessagesPlaceholder(variable_name="messages", optional=True),
 ])
 
+class AskUser(BaseModel):
+    ask: str
+
+def user_answer(state):
+    pass
+
+
 def generate_user_profile_question(state: State):
     gen_user_profile_chain = gen_user_profile | llm
     result = gen_user_profile_chain.invoke(state)
-    print(f"AI: {result.content}")
-    return {"message" : [result]}
+    return {"messages" : [result]}
 
-def human_node(state: State):
-    user_input = input("User: ")
-    return {"messages" : [HumanMessage(content=user_input)]}
 
-def end_user_profile_cond(state: State):
-    last_msg = state["messages"][-1].content
-    if "Thank you so much for your patience" in last_msg:
-        print(last_msg)
+def router(state: State):
+    last_msg = state["messages"][-1]
+    if "Thank you so much for your patience" in last_msg.content:
+        last_msg.pretty_print()
         return END
     else:
-        return "gen_question"
+        return "user_answer"
+
 
 graph = StateGraph(state_schema=State)
 graph.add_node("gen_question", generate_user_profile_question)
-graph.add_node("human_answer", human_node)
+graph.add_node("user_answer", user_answer)
 graph.add_edge(START , "gen_question")
-graph.add_edge("gen_question", "human_answer")
-graph.add_conditional_edges("human_answer", end_user_profile_cond)
+graph.add_conditional_edges("gen_question", router)
+graph.add_edge("user_answer", "gen_question")
 memory = SqliteSaver.from_conn_string(":memory:")
-g = graph.compile(checkpointer=memory)
+g = graph.compile(checkpointer=memory, interrupt_before=["user_answer"])
 config = {"configurable": {"thread_id": "1"}}
 # img = g.get_graph().draw_png()
 
-init_state = {
-    "messages": [
-        HumanMessage(
-            content=f"{user_question}?",
-        )
-    ],
-}
-print(init_state)
+# events是一个迭代器，只有被取出的时候图才会开始执行，每次图执行到某个节点都需要新的state输入
+# next(events)
+for event in g.stream({"messages": [("user", user_question)]}, config, stream_mode="values"):
+    event["messages"][-1].pretty_print()
 
+user_answer = input("User: ")
 
-for step in g.stream(init_state, config):
-    print(step)
-    # name = next(iter(step))
-    # print(name)
-    # print("-- ", str(step[name]["messages"])[:300])
-    # if END in step:
-    #     final_step = step
-        
+g.update_state(config, {"messages": ("user", user_answer)}, as_node="user_answer")
+events = g.stream(None, config, stream_mode="values")
 
+for event in events:
+    event["messages"][-1].pretty_print()
